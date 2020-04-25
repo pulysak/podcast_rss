9c69b6f68ca038388d582837f125abd4fecae0b0
p.lysak <p.lysak@cian.ru>
2020-04-25 17:59:15 +0300

    master -- update url names

--- show/urls.py
+++ show/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:show_id>/apple_feed/', views.ShowAppleFeed(), name='show_rss'),
-    path('<int:show_id>/spotify_feed/', views.ShowSpotifyFeed(), name='show_rss'),
+    path('<int:show_id>/apple_feed/', views.ShowAppleFeed(), name='show_apple_rss'),
+    path('<int:show_id>/spotify_feed/', views.ShowSpotifyFeed(), name='show_spotify_rss'),
 ]
